d8835e08ce1a97fd1818aa8ff3e5c5b1acf5a50d
Kanhaiya Kumar <kanhaiya5kumar@gmail.com>
2022-05-29 22:47:46 -0700

    increament&decreament programme

---
+++ increament_decreament.java
@@ -0,0 +1,13 @@
+public class increament_decreament
+{
+    public static void main(String[] args) 
+    {
+     int x=0;
+     System.out.println("increamnet _ decreament programme");
+     System.out.println("-------------------------------------");   
+     for(int i=0;i<=100;i++)
+     {
+     System.out.print(i+" ");
+     }
+    }
+}
