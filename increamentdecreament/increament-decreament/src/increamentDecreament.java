935800b9ba29e3d1bfddb82a91f5c1f8544ae08f
Kanhaiya Kumar <kanhaiya5kumar@gmail.com>
2022-05-29 23:40:19 -0700

    increament decreament operators

---
+++ increamentdecreament/increament-decreament/src/increamentDecreament.java
@@ -0,0 +1,17 @@
+
+public class increamentDecreament
+{
+
+	public static void main(String[] args)
+	{
+		int x=0;
+	     System.out.println("increamnet _ decreament programme");
+	     System.out.println("-------------------------------------");   
+	     for(int i=0;i<=100;i++)
+	     {
+	     System.out.print(i+" ");
+	     }
+	     System.out.println("------------------------------------------");
+	}
+
+}
